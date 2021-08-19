d9cf735a514b3e06bcb67d38601fd67c71fef128
Kath Pobee-Norris <kath.pobee-norris@citizensadvice.org.uk>
2021-08-19 12:39:09 +0100

    Stop creating entries for internal links
    One of the biggest issues we have during the migration is when trying to
    publish content and getting the error "linked entry is missing or
    inaccessible".  When we hit the Contentful converter it would create an
    entry, rather than just a plain hyperlink.  This means when we're trying
    to publish a page Contentful is looking for an entry which doesn't
    actually exist, and prevents us from publishing.
    
    We can also end up with empty links and this too was creating an
    entry-link with no ID.  I think it would be better to just leave these
    as hyperlinks to avoid Contentful looking for an entry without an ID.

--- lib/contentful_converter/nodes/hyperlink.rb
+++ lib/contentful_converter/nodes/hyperlink.rb
@@ -6,9 +6,12 @@ require 'uri'
 module ContentfulConverter
   module Nodes
     class Hyperlink < Base
+      SECTIONS = %w[family immigration debt-and-money law-and-courts consumer benefits health housing work about-us]
       private
 
       def type
+        return 'hyperlink' if parsed_link.to_s.empty?
+        return 'hyperlink' if internal_link?
         return 'asset-hyperlink' if !uri_scheme? && uri_extension?
         return 'entry-hyperlink' if !uri_scheme? && !parsed_link.to_s.include?("#")
 
@@ -16,6 +19,8 @@ module ContentfulConverter
       end
 
       def options
+        return hyperlink_option if parsed_link.to_s.empty?
+        return hyperlink_option if internal_link?
         return hyperlink_entry_option('Asset') if !uri_scheme? && uri_extension?
         return hyperlink_entry_option('Entry') if !uri_scheme? && !parsed_link.to_s.include?("#")
 
@@ -64,6 +69,17 @@ module ContentfulConverter
 
       def link_value
         nokogiri_node[:href]
+      end
+
+      def internal_link?
+        return if parsed_link.path.nil?
+
+        section = parsed_link.to_s.delete_prefix("https://www.citizensadvice.org.uk/")
+        section = section.delete_prefix("/")
+        section = section.delete_prefix("advisernet/")
+        section = section.split("/").compact.first
+
+        SECTIONS.include?(section) || section == "scotland" || section == "wales"
       end
     end
   end
